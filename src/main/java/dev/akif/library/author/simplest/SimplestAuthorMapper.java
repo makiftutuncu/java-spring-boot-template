e5ecbfb2f1590aa316f85a0648a8a9b7fe6bc7bd
Mehmet Akif Tütüncü <m.akif.tutuncu@gmail.com>
2023-02-27 13:40:11 +0100

    Remove obsolete implementations in mappers

--- src/main/java/dev/akif/library/author/simplest/SimplestAuthorMapper.java
+++ src/main/java/dev/akif/library/author/simplest/SimplestAuthorMapper.java
@@ -3,26 +3,10 @@ package dev.akif.library.author.simplest;
 import dev.akif.crud.simplest.SimplestMapper;
 import org.springframework.stereotype.Component;
 
-import java.time.Instant;
 import java.util.UUID;
 
 @Component
 public class SimplestAuthorMapper implements SimplestMapper<UUID, SimplestAuthorEntity> {
-    @Override
-    public SimplestAuthorEntity createDTOToCreateModel(final SimplestAuthorEntity authorEntity) {
-        return new SimplestAuthorEntity(authorEntity.getName(), authorEntity.getBirthDate(), null);
-    }
-
-    @Override
-    public SimplestAuthorEntity updateDTOToUpdateModel(final SimplestAuthorEntity authorEntity) {
-        return new SimplestAuthorEntity(authorEntity.getName(), authorEntity.getBirthDate(), null);
-    }
-
-    @Override
-    public SimplestAuthorEntity entityToBeCreatedFrom(final SimplestAuthorEntity authorEntity, final Instant now) {
-        return new SimplestAuthorEntity(authorEntity.getName(), authorEntity.getBirthDate(), now);
-    }
-
     @Override
     public void updateEntityWith(final SimplestAuthorEntity updateAuthorEntity, final SimplestAuthorEntity authorEntity) {
         authorEntity.setName(updateAuthorEntity.getName());
